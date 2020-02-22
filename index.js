6df035d77ece9ab90a3b5b6bd3595a07f8e283f5
Tina Hang <mstinahang@gmail.com>
2020-02-22 16:43:13 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  const win = array.find( o => o['result'] === "W");
+  if (!win) {
+    return undefined;
+  } else {
+    return win['year'];
+  };
+}
